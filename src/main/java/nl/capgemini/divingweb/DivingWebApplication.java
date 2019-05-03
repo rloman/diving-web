0ca50073b2ebc5b9b4a30b23bd6a4183d8f118e7
Raymond Loman <raymond@carpago.nl>
2019-05-03 14:05:15 +0200

    Initial load

---
+++ src/main/java/nl/capgemini/divingweb/DivingWebApplication.java
@@ -0,0 +1,13 @@
+package nl.capgemini.divingweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DivingWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DivingWebApplication.class, args);
+	}
+
+}
