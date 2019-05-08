3308a7c7c4a2f85fda67506d64e590d1f983ab52
Raymond Loman <raymond@carpago.nl>
2019-05-08 14:22:27 +0200

    Add (working) Security to the Diving Web app

---
+++ src/main/java/nl/capgemini/divingweb/config/SecurityConfig.java
@@ -0,0 +1,54 @@
+package nl.capgemini.divingweb.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+
+// see
+// http://docs.spring.io/spring-security/site/docs/3.2.x/reference/htmlsingle/#multiple-httpsecurity
+
+@Configuration
+@EnableGlobalMethodSecurity(securedEnabled = true)
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    public static final String APIROLE = "APIROLE";
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth,
+                                @Value("${api.username}") String apiUsername,
+                                @Value("${api.password}") String apiPassword) throws Exception {
+
+        auth.
+                inMemoryAuthentication()
+                .passwordEncoder(NoOpPasswordEncoder.getInstance())
+                .withUser(apiUsername)
+                .password(apiPassword)
+                .roles(APIROLE);
+    }
+
+    @Configuration
+    @Order(1)
+    public static class AuWebSecurityAdapterRest extends WebSecurityConfigurerAdapter {
+
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            http.csrf().disable();
+            http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+            http.authorizeRequests().antMatchers("/api/**").hasRole(APIROLE).and().httpBasic();
+
+            http.authorizeRequests().anyRequest().permitAll();
+
+            // the rest is implicit denied
+        }
+    }
+
+}
